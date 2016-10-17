d913946e3b69d268d64538b548bf09c9ccdf9ab3
JPD <vepiteski@gmail.com>
2016-10-16 21:47:50 -0400

    clean bkp files

--- src/ARCSpectral.jl
+++
@@ -1,8 +0,0 @@
-fname = :ARCSpectral
-c = JPDJPD.Combi(JPDJPD.hessian_dense,JPDJPD.PDataSpectral,JPDJPD.solve_modelARCDiag,JPDJPD.preprocessSpectral,JPDJPD.decreaseFact,JPDJPD.Tparam())
-@eval begin
-    export $fname
-
-
-    include("Template.jl")
-end
--- src/Solvers/ARCqKdense.jl~
+++
@@ -1,3 +0,0 @@
-fname = :ARCqKsparse
-c = Combi(hessian_sparse,PDataK,solve_modelKARC,preprocessKARC,decreaseKARC,TparamsKARC())
-include("Template.jl")
