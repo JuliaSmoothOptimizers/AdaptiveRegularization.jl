b1cbe93f0fd524ebcf33b1ff76d2e3f2580bd743
JPD <vepiteski@gmail.com>
2016-10-18 09:05:52 -0400

    deps

--- deps/install.jl
+++ deps/install.jl
@@ -5,10 +5,8 @@ import Compat.String
 const home = "https://github.com/JuliaSmoothOptimizers"
 const deps = Dict{String, String}(
               "Krylov" => "develop"
-)
+              )
 
-const unix_deps = Dict{String, String}(
-              "CUTEst" => "develop")
 
 function dep_installed(dep)
   try
@@ -32,4 +30,3 @@ function deps_install(deps)
 end
 
 deps_install(deps)
-#@static if is_unix() deps_install(unix_deps); end
