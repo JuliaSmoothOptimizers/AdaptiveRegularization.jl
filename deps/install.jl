17469191787579a37f7750d5abc3cb4293ab74e7
JPD <vepiteski@gmail.com>
2016-10-18 08:59:28 -0400

    deps

---
+++ deps/install.jl
@@ -0,0 +1,35 @@
+Pkg.add("Compat")
+using Compat
+import Compat.String
+
+const home = "https://github.com/JuliaSmoothOptimizers"
+const deps = Dict{String, String}(
+              "Krylov" => "develop"
+)
+
+const unix_deps = Dict{String, String}(
+              "CUTEst" => "develop")
+
+function dep_installed(dep)
+  try
+    Pkg.installed(dep)  # throws an error instead of returning false
+    return true
+  catch
+    return false
+  end
+end
+
+function dep_install(dep, branch)
+  dep_installed(dep) || Pkg.clone("$home/$dep.jl.git")
+  Pkg.checkout(dep, branch)
+  Pkg.build(dep)
+end
+
+function deps_install(deps)
+  for dep in keys(deps)
+    dep_install(dep, deps[dep])
+  end
+end
+
+deps_install(deps)
+#@static if is_unix() deps_install(unix_deps); end
