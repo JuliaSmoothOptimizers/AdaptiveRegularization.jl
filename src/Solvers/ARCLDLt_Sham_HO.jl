c4ef514d52733a905665d65f019028e66f16ad62
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2019-11-03 20:38:45 -0500

    small change to accept kwargs...

--- src/Solvers/ARCLDLt_Sham_HO.jl
+++ src/Solvers/ARCLDLt_Sham_HO.jl
@@ -2,6 +2,7 @@ export ARCLDLt_HO_Sham
 
 function ARCLDLt_HO_Sham(nlp 		:: AbstractNLPModel,
               			nlpstop 	:: NLPStopping;
+						λfact = 100.0,
 						kwargs...
                			)
 
@@ -10,7 +11,7 @@ function ARCLDLt_HO_Sham(nlp 		:: AbstractNLPModel,
 	return TRARC(nlp,
 				  nlpstop;
 				  TR = TrustRegion(T(10.0)),
-				  c = Combi(hessian_dense, PDataLDLt{eltype(nlp.meta.x0)}, solve_modelARCDiag_HO, preprocessLDLt, decreaseFact, Tparam{eltype(nlp.meta.x0)}()),
+				  c = Combi(hessian_dense, PDataLDLt{eltype(nlp.meta.x0)}, (x, y, z) -> solve_modelARCDiag_HO(x, y, z, ho_correction = :Shamanskii, λfact = λfact), preprocessLDLt, decreaseFact, Tparam{eltype(nlp.meta.x0)}()),
 				  kwargs...
 				  )
 end
