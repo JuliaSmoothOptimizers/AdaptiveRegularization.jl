2ba0ba09713b8c6a45aae4afcfb7d93d968a0b36
tmigot <tangi.migot@gmail.com>
2022-03-10 20:33:45 +0100

    refresh solvers include

--- src/ARCTR.jl
+++ src/ARCTR.jl
@@ -13,6 +13,25 @@ include("Includes.jl")
 
 include("TRARC.jl")
 
-include("Solvers/solvers.jl")
+####################################################################
+# Include solvers
+export ALL_solvers
+
+ALL_solvers = Function[]
+
+path = joinpath(dirname(@__FILE__), "Solvers")
+files = filter(x -> x[(end - 2):end] == ".jl", readdir(path))
+for file in files
+    if occursin(r"MA", file)
+        continue # Remove MA57 and MA97 solvers for now
+    end
+    if file in ["ARCqKOp05.jl", "ARCqKOp2.jl", "TRLDLt_HO_MP.jl"] #use something else than TRARC
+        continue
+    end
+    include("Solvers/" * file)
+    push!(ALL_solvers, eval(Symbol(split(file, ".")[1])))
+    @show file, split(file, ".")[1]
+end
+####################################################################
 
 end # module
