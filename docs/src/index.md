1e5a42987763f04f13f20aebca5446708ceac909
tmigot <tangi.migot@gmail.com>
2022-03-17 23:43:24 +0100

    remove BK version

--- docs/src/index.md
+++ docs/src/index.md
@@ -21,8 +21,8 @@ The package contains several implementations:
 -[X] update for in-place preprocess functions
 -[X] update for in-place `hessian_rep` functions
 -[X] we should probably move decreaseKTR and decreaseKARC here in Utilies.jl
--[ ] Unify `TParams.shifts` and in `PData.shifts`
--[ ] Why is there a LDLt and LDLt2? LDLt2 that depend on a `bunchkaufman` function (from LinearAlgebra?)
+-[X] Unify `TParams.shifts` and in `PData.shifts`
+-[X] Why is there a LDLt and LDLt2? LDLt2 that depend on a `bunchkaufman` function (from LinearAlgebra?)
 -[X] we could use multiple-dispatch and have just one function `decrease` and `increase`.
 -[X] remove `reconstructH` as it is nowhere used.
 -[ ] extract parameters in KARC and KTR
--- src/PreProcess/PreProcessLDLt.jl
+++ src/PreProcess/PreProcessLDLt.jl
@@ -1,4 +1,4 @@
-export preprocessLDLt, preprocessLDLt2
+export preprocessLDLt
 function preprocessLDLt(PData::PDataLDLt, H, g, n1, n2)
     # printstyled("dans preprocessLDLt ⤈ \n", color = :red)
     T = eltype(H)
@@ -84,91 +84,6 @@ function preprocessLDLt(PData::PDataLDLt, H, g, n1, n2)
     λ = max(-l_m, 0.0)
     # @show eltype(λ)
     # @show eltype(λ)
-    # @show λ
-    # printstyled("avant de sortir de PreProcessLDLt ↑ \n", color = :red)
-
-    PData.L = L
-    PData.D = D
-    PData.pp = pp
-    PData.Δ = Δ
-    PData.Q = Q
-    PData.g̃ = g̃
-    PData.λ = λ
-    PData.success = true
-    PData.OK = true
-
-    return PData
-end
-
-function preprocessLDLt2(PData::PDataLDLt, H, g, n1, n2)
-    # printstyled("dans preprocessLDLt \n", color = :red)
-    T = eltype(H)
-    # @show H
-    # @show g
-    n, = size(g)
-    # @show n
-    global L = Matrix{T}(undef, n, n)
-    global D = Matrix{T}(undef, n, n)
-    global pp = Vector{Int}(undef, n)
-    global ρ = nothing
-    global ncomp = nothing
-    global S = nothing
-
-    # @show H
-    # printstyled("ici ↩ \n", color = :red)
-
-    try
-        # (L, D, pp, ρ, ncomp) = ldlt_symm(H, 'r')
-        # @show H
-        S = bunchkaufman(Symmetric(H, :L), true, check = false)
-    catch
-        println("*******   Problem in LDLt")
-        res = PDataLDLt()
-        res.OK = false
-        return res
-    end
-    L = S.L
-    D = S.D
-    pp = S.p
-
-    # printstyled("dans preprocessLDLt apres ldlt_symm eltype(L) = $(eltype(L)) \n", color = :cyan)
-    # @show L
-    # @show D
-    # @show pp
-    # @show ρ
-    # @show ncomp
-
-    if true in isnan.(D)
-        println("*******   Problem in D from LDLt: NaN")
-        println(" cond (H) = $(cond(H))")
-        res = PDataLDLt()
-        res.OK = false
-        return res
-    end
-
-    # @show L
-    # @show D
-    # @show pp
-
-    D = Hermitian(D)
-    X = eigen(Matrix(D))
-    Δ = X.values
-    # @show Δ
-    Q = X.vectors
-    l_m, = findmin(Δ)
-    # @show l_m
-    ĝ = L \ (g[pp])
-    # @show ĝ[1]
-    # @show ĝ[2]
-    # @show eltype(ĝ)
-    g̃ = Q' * ĝ
-    # @show g̃[1]
-    # @show g̃[2]
-    # @show eltype(g̃)
-
-    n_g = norm(g)
-    # @show n_g
-    λ = max(-l_m, 0.0)
     # @show λ
     # printstyled("avant de sortir de PreProcessLDLt ↑ \n", color = :red)
 
