215fe756284d0af56637fbb3b8c102bfc9af5e04
tmigot <tangi.migot@gmail.com>
2022-06-06 20:43:42 +0200

    Remove allocations in solve (#25)
    * remove allocations in SolveKARC
    
    * remove allocs in SolveKTR

--- src/SolveModel/SolveModelKARC.jl
+++ src/SolveModel/SolveModelKARC.jl
@@ -1,15 +1,22 @@
 function solve_modelKARC(H, g, nlp_stop, X::PDataKARC, α::T) where {T}
     # target value should be close to satisfy αλ=||d||
-    target = [(abs(α * X.shifts[i] - X.norm_dirs[i])) for i = 1:X.nshifts]
-    positives = findall(X.positives)
+    start = findfirst(X.positives)
+    if isnothing(start)
+        start = 1
+    end
+    if VERSION < v"1.7.0"
+        positives = collect(start:length(X.positives))
+        target = [(abs(α * X.shifts[i] - X.norm_dirs[i])) for i in positives]
+    else
+        positives = start:length(X.positives)
+        target = ((abs(α * X.shifts[i] - X.norm_dirs[i])) for i in positives)
+    end
 
     # pick the closest shift to the target within positive definite H+λI
-    X.indmin = max(1, X.indmin)
-    start = X.indmin
-    bidon, indmin = findmin(target[positives[start:end]])
+    indmin = argmin(target)
     X.indmin = start + indmin - 1
-    p_imin = positives[X.indmin]
-    X.d = X.xShift[p_imin]
+    p_imin = X.indmin
+    X.d .= X.xShift[p_imin]
     X.λ = X.shifts[p_imin]
 
     return X.d, X.λ
--- src/SolveModel/SolveModelKTR.jl
+++ src/SolveModel/SolveModelKTR.jl
@@ -1,24 +1,32 @@
 export solve_modelKTR
 function solve_modelKTR(H, g, nlp_stop, X::PDataKTR, α::T) where {T}
     # target value should be close to satisfy α=||d||
-    target = [(abs(α - X.norm_dirs[i])) for i = 1:X.nshifts]
+    start = findfirst(X.positives)
+    if isnothing(start)
+        start = 1
+    end
+    if VERSION < v"1.7.0"
+        positives = collect(start:length(X.positives))
+        target = [(abs(α * X.shifts[i] - X.norm_dirs[i])) for i in positives]
+    else
+        positives = start:length(X.positives)
+        target = ((abs(α * X.shifts[i] - X.norm_dirs[i])) for i in positives)
+    end
     # pick the closest shift to the target within positive definite H+λI
     # before, check the shift = 0 for direction within the trust region
 
-    positives = findall(X.positives)
     if (X.indmin == 0)  #  try Newton's direction
         X.indmin = 1
-        if (positives[1] == 1) & (X.norm_dirs[1] <= α)
-            X.d = X.xShift[1]
-            X.λ = 0.0
+        if (start == 1) & (X.norm_dirs[1] <= α)
+            X.d .= X.xShift[1]
+            X.λ = zero(T)
             return X.d, X.λ
         end
     end
-    start = X.indmin
-    bidon, indmin = findmin(target[positives[start:end]])
+    indmin = argmin(target)
     X.indmin = start + indmin - 1
-    p_imin = positives[X.indmin]
-    X.d = X.xShift[p_imin]
+    p_imin = X.indmin
+    X.d .= X.xShift[p_imin]
     # Refine this rough constrained direction
     X.λ = X.shifts[p_imin]
 
