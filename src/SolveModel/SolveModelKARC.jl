4824b50d608b24bcc358f03f0804ba47d600c6b9
tmigot <tangi.migot@gmail.com>
2022-06-06 10:56:46 +0200

    rmv comments

--- src/SolveModel/SolveModelKARC.jl
+++ src/SolveModel/SolveModelKARC.jl
@@ -1,7 +1,5 @@
 function solve_modelKARC(H, g, nlp_stop, X::PDataKARC, α::T) where {T}
     # target value should be close to satisfy αλ=||d||
-    # target = [(abs(α * X.shifts[i] - X.norm_dirs[i])) for i = 1:X.nshifts]
-    # positives = findall(X.positives)
     start = findfirst(X.positives)
     if isnothing(start)
         start = 1
@@ -10,9 +8,9 @@ function solve_modelKARC(H, g, nlp_stop, X::PDataKARC, α::T) where {T}
     target = ((abs(α * X.shifts[i] - X.norm_dirs[i])) for i in positives)
 
     # pick the closest shift to the target within positive definite H+λI
-    indmin = argmin(target) # findmin(target[positives[start:end]])
+    indmin = argmin(target)
     X.indmin = start + indmin - 1
-    p_imin = X.indmin # Is there a diff with findall(X.positives)[X.indmin] ?
+    p_imin = X.indmin
     X.d .= X.xShift[p_imin]
     X.λ = X.shifts[p_imin]
 
