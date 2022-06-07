42ff514b2084d010fb683203be2385a1e9e80a69
tmigot <tangi.migot@gmail.com>
2022-06-07 18:46:38 +0200

    Add SolverTest and multiprecision (#31)
    * add solvertest
    
    * fix multiprecision tests

--- src/SolveModel/SolveModelST_TR.jl
+++ src/SolveModel/SolveModelST_TR.jl
@@ -1,4 +1,4 @@
-function solve_modelST_TR(H, g, nlp_stop, PData::PDataST, δ::T; cgtol::T = 0.1) where {T}
+function solve_modelST_TR(H, g, nlp_stop, PData::PDataST, δ::T) where {T}
     # cas particulier Steihaug-Toint
     # ϵ = sqrt(eps(T)) # * 100.0 # old
     # cgtol = max(ϵ, min(cgtol, 9 * cgtol / 10, 0.01 * norm(g)^(1.0 + PData.ζ))) # old
