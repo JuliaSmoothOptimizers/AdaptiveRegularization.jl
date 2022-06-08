6a556eb834319a2d80abac27fb11b0aae5876aef
tmigot <tangi.migot@gmail.com>
2022-06-08 10:59:27 +0200

    add `docstring` and `TRARC(nlp)` function

--- src/ARCTR.jl
+++ src/ARCTR.jl
@@ -33,6 +33,62 @@ ALL_solvers = Symbol[]
 
 include("solvers.jl")
 
+export TRARC
+
+"""
+    TRARC(nlp; kwargs...)
+
+Compute a local minimum of an unconstrained optimization problem using trust-region (TR)/adaptive regularization with cubics (ARC) methods.
+# Arguments
+- `nlp::AbstractNLPModel`: the model solved, see `NLPModels.jl`.
+The keyword arguments include
+- `TR::TrustRegion`: structure with trust-region/ARC parameters, see [`TrustRegion`](@ref). Default: `TrustRegion(T(10.0))`.
+- `hess_type::Type{Hess}`: Structure used to handle the hessian. The possible values are: `HessDense`, `HessSparse`, `HessSparseCOO`, `HessOp`. Default: `HessOp`.
+- `pdata_type::Type{ParamData}` Structure used for the preprocessing step. Default: `PDataKARC`.
+- `solve_model::Function` Function used to solve the subproblem. Default: `solve_modelKARC`.
+- `robust::Bool`: `true` implements a robust evaluation of the model. Default: `true`.
+- `verbose::Bool`: `true` prints iteration information. Default: `false`.
+Additional `kwargs` are used for stopping criterion, see `Stopping.jl`.
+
+# Output
+The returned value is a `GenericExecutionStats`, see `SolverCore.jl`.
+
+This implementation uses `Stopping.jl`. Therefore, it is also possible to used
+        
+    TRARC(stp; kwargs...)
+
+which returns the `stp::NLPStopping` updated.
+
+For advanced usage, the principal call to the solver uses a [`TRARCWorkspace`](@ref).
+
+    TRARC(stp, pdata, workspace, trust_region_parameters; kwargs...)
+
+Some variants of TRARC are already implemented and listed in `ARCTR.ALL_solvers`.
+
+# References
+This method unifies the implementation of trust-region and adaptive regularization with cubics as described in
+
+    Dussault, J.-P. (2020).
+    A unified efficient implementation of trust-region type algorithms for unconstrained optimization.
+    INFOR: Information Systems and Operational Research, 58(2), 290-309.
+    10.1080/03155986.2019.1624490
+
+# Examples
+```jldoctest
+julia> using ARCTR, ADNLPModels
+julia> nlp = ADNLPModel(x -> 100 * (x[2] - x[1]^2)^2 + (x[1] - 1)^2, [-1.2; 1.0]);
+julia> stats = TRARC(nlp)
+"Execution stats: first-order stationary"
+```
+"""
+function TRARC end
+
+function TRARC(nlp::AbstractNLPModel{T,S}; kwargs...) where {T,S}
+    nlpstop = NLPStopping(nlp; optimality_check = (pb, state) -> norm(state.gx), kwargs...)
+    nlpstop = TRARC(nlpstop; kwargs...)
+    return stopping_to_stats(nlpstop)
+end
+
 for fun in keys(solvers_const)
     push!(ALL_solvers, fun)
 
@@ -51,13 +107,7 @@ for fun in keys(solvers_const)
     end
     @eval begin
         function $fun(nlp::AbstractNLPModel{T,S}; kwargs...) where {T,S}
-            # https://github.com/vepiteski/Stopping.jl/blob/4036e996be4e9521bb40725a659a03d23fd70e03/src/Stopping/nlp_admissible_functions.jl#L12
-            nlpstop = NLPStopping(
-                nlp;
-                optimality_check = (pb, state) ->
-                    unconstrained_check(pb, state, pnorm = T(2)),
-                kwargs...,
-            )
+            nlpstop = NLPStopping(nlp; optimality_check = (pb, state) -> norm(state.gx), kwargs...)
             nlpstop = $fun(nlpstop; kwargs...)
             return stopping_to_stats(nlpstop)
         end
