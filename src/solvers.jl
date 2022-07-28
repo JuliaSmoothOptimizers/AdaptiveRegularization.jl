2e44496f1d928b01056762ffd933b56a4069a849
tmigot <tangi.migot@gmail.com>
2022-07-29 01:07:37 +0200

    add `lsqr` solver

--- src/solvers.jl
+++ src/solvers.jl
@@ -39,6 +39,7 @@ const solvers_nls_const = Dict(
         [:shifts => 10.0 .^ (collect(-10.0:0.5:20.0))],
     ),
     :ST_TROpGN => (HessGaussNewtonOp, PDataST, solve_modelST_TR, ()),
-    :ST_TROpGNLS => (HessGaussNewtonOp, PDataNLSST, solve_modelNLSST_TR, ()),
+    :ST_TROpGNLSCgls => (HessGaussNewtonOp, PDataNLSST, solve_modelNLSST_TR, [:solver_method => :cgls]),
+    :ST_TROpGNLSLsqr => (HessGaussNewtonOp, PDataNLSST, solve_modelNLSST_TR, [:solver_method => :lsqr]),
     :ST_TROpLS => (HessOp, PDataNLSST, solve_modelNLSST_TR, ()),
 )
