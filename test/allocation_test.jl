3c84d302398c59ac9fe11ae4a13fa3d00cabbcb2
tmigot <tangi.migot@gmail.com>
2023-12-21 09:41:24 +0100

    improve allocation tests

--- test/allocation_test.jl
+++ test/allocation_test.jl
@@ -1,25 +1,4 @@
 using AdaptiveRegularization, LinearAlgebra, Test
-
-T = Float64
-S = Vector{T}
-n = 1000
-
-TR = TrustRegion(T(10))
-α = T(100)
-
-for XData in (PDataKARC(S, T, n), PDataTRK(S, T, n), PDataST(S, T, n))
-  @testset "Allocation test in AdaptiveRegularization.decrease for $(typeof(XData))" begin
-    alloc_decrease() = @allocated AdaptiveRegularization.decrease(XData, α, TR)
-    alloc_decrease()
-    @test alloc_decrease() <= 16
-  end
-  @testset "Allocation test in AdaptiveRegularization.decrease for $(typeof(XData))" begin
-    alloc_increase() = @allocated AdaptiveRegularization.increase(XData, α, TR)
-    alloc_increase()
-    @test (@allocated alloc_increase()) <= 16
-  end
-end
-
 using ADNLPModels, NLPModels, OptimizationProblems, Stopping
 
 nlp = OptimizationProblems.ADNLPProblems.arglina()
@@ -31,26 +10,31 @@ g = grad(nlp, x)
 ng = norm(g)
 calls, max_calls = 0, 1000000
 
-for (Data, solve, limit_solve, limit_preprocess) in (
-  (PDataKARC, :solve_modelKARC, 144, 0),
-  (PDataTRK, :solve_modelTRK, 144, 0),
-  (PDataST, :solve_modelST_TR, 224, 0),
+function alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
+  AdaptiveRegularization.preprocess(XData, H, g, ng, calls, max_calls, α)
+  return nothing
+end
+
+function alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
+  solve(XData, H, g, ng, calls, max_calls, α)
+  return nothing
+end
+
+for (Data, solve) in (
+  (PDataKARC, AdaptiveRegularization.solve_modelKARC),
+  (PDataTRK, AdaptiveRegularization.solve_modelTRK),
+  (PDataST, AdaptiveRegularization.solve_modelST_TR),
 )
+  XData = Data(S, T, n)
   @testset "Allocation test in preprocess with $(Data)" begin
-    XData = Data(S, T, n)
-    alloc_preprocess(XData, H, g, ng, calls, max_calls, α) =
-      @allocated AdaptiveRegularization.preprocess(XData, H, g, ng, calls, max_calls, α)
     alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
-    @test alloc_preprocess(XData, H, g, ng, calls, max_calls, α) <= limit_preprocess
-    @show alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
+    al = @allocated alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
+    @test al == 0
   end
 
   @testset "Allocation test in $solve with $(Data)" begin
-    XData = Data(S, T, n)
-    alloc_solve_model(XData, H, g, ng, calls, max_calls, α) =
-      @allocated AdaptiveRegularization.eval(solve)(XData, H, g, ng, calls, max_calls, α)
-    alloc_solve_model(XData, H, g, ng, calls, max_calls, α)
-    @test alloc_solve_model(XData, H, g, ng, calls, max_calls, α) <= limit_solve
-    @show alloc_solve_model(XData, H, g, ng, calls, max_calls, α)
+    alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
+    al = @allocated alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
+    @test al == 0
   end
 end
---
+++ test/allocation_test_utils.jl
@@ -0,0 +1,58 @@
+using AdaptiveRegularization, LinearAlgebra, Test
+
+T = Float64
+S = Vector{T}
+n = 1000
+
+TR = TrustRegion(T(10))
+α = T(100)
+
+# Test increase / decrease
+
+function alloc_decrease(XData, α, TR)
+  AdaptiveRegularization.decrease(XData, α, TR)
+  return nothing
+end
+
+function alloc_increase(XData, α, TR)
+  AdaptiveRegularization.increase(XData, α, TR)
+  return nothing
+end
+
+for XData in (PDataKARC(S, T, n), PDataTRK(S, T, n), PDataST(S, T, n))
+  @testset "Allocation test in AdaptiveRegularization.decrease for $(typeof(XData))" begin
+    alloc_decrease(XData, α, TR)
+    al = @allocated alloc_decrease(XData, α, TR)
+    @test al == 0
+  end
+  @testset "Allocation test in AdaptiveRegularization.decrease for $(typeof(XData))" begin
+    alloc_increase(XData, α, TR)
+    al = @allocated alloc_increase(XData, α, TR)
+    @test al == 0
+  end
+end
+
+# Test hessian evaluation in-place
+
+using NLPModelsTest
+nlp = NLPModelsTest.BROWNDEN()
+n = nlp.meta.nvar
+x0 = nlp.meta.x0
+
+function alloc_hessian(who, nlp, x0)
+  AdaptiveRegularization.hessian!(who, nlp, x0)
+  return nothing
+end
+
+@testset "Test in-place hessian allocations" for (Workspace, limit) in (
+  (HessDense, 1952),
+  (HessSparse, 944),
+  (HessSparseCOO, 0),
+  (HessOp, 960),
+)
+  who = Workspace(nlp, n)
+  alloc_hessian(who, nlp, x0)
+  al = @allocated alloc_hessian(who, nlp, x0)
+  @test al <= limit
+  println("Allocations for $Workspace is $al.")
+end
