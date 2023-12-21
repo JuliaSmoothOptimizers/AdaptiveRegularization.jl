367016cadcb258f2b9d6ba06d0dcd876ecaf0fc2
tmigot <tangi.migot@gmail.com>
2023-12-21 11:18:21 +0100

    improve further test rendering

--- test/allocation_test.jl
+++ test/allocation_test.jl
@@ -20,21 +20,23 @@ function alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
   return nothing
 end
 
-@testset "Allocation test in preprocess and solvemodel" for (Data, solve) in (
-  (PDataKARC, AdaptiveRegularization.solve_modelKARC),
-  (PDataTRK, AdaptiveRegularization.solve_modelTRK),
-  (PDataST, AdaptiveRegularization.solve_modelST_TR),
-)
-  XData = Data(S, T, n)
-  @testset "Allocation test in preprocess with $(Data)" begin
-    alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
-    al = @allocated alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
-    @test al == 0
-  end
+@testset "Allocation test in preprocess and solvemodel" begin
+  for (Data, solve) in (
+    (PDataKARC, AdaptiveRegularization.solve_modelKARC),
+    (PDataTRK, AdaptiveRegularization.solve_modelTRK),
+    (PDataST, AdaptiveRegularization.solve_modelST_TR),
+  )
+    XData = Data(S, T, n)
+    @testset "Allocation test in preprocess with $(Data)" begin
+      alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
+      al = @allocated alloc_preprocess(XData, H, g, ng, calls, max_calls, α)
+      @test al == 0
+    end
 
-  @testset "Allocation test in $solve with $(Data)" begin
-    alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
-    al = @allocated alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
-    @test al == 0
+    @testset "Allocation test in $solve with $(Data)" begin
+      alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
+      al = @allocated alloc_solve_model(solve, XData, H, g, ng, calls, max_calls, α)
+      @test al == 0
+    end
   end
 end
