358f5d4b7802371689a91ae61077ea5036aa3cf9
tmigot <tmigot@users.noreply.github.com>
2023-12-24 08:36:01 +0000

    :robot: Format .jl files

--- paper/benchmark_arctr.jl
+++ paper/benchmark_arctr.jl
@@ -28,8 +28,8 @@ rtol = 1e-6
 
 eL = 10.0
 eU = 20.0
-ψ = 10 ^ (1 // 2)
-shifts = 10.0 .^ (collect(-eL:0.5:eU))
+ψ = 10^(1 // 2)
+shifts = 10.0 .^ (collect((-eL):0.5:eU))
 
 ζ = 0.5
 
@@ -38,41 +38,42 @@ shifts = 10.0 .^ (collect(-eL:0.5:eU))
 η₁ = 0.1
 η₂ = 0.75
 TR = TrustRegion(
-    10;
-    acceptance_threshold = η₁,
-    increase_threshold = η₂,
-    increase_factor = γ₂,
-    decrease_factor = γ₁,
+  10;
+  acceptance_threshold = η₁,
+  increase_threshold = η₂,
+  increase_factor = γ₂,
+  decrease_factor = γ₁,
 )
 
 solvers = Dict(
-    :ARCqKOp =>
-        nlp -> ARCqKOp(
-            nlp,
-            TR = TR,
-            shifts = shifts,
-            verbose = false,
-            atol = atol,
-            rtol = rtol,
-            max_time = max_time,
-            max_iter = max_iter,
-            ζ = ζ,
-        ),
-    :ST_TROp =>
-        nlp -> ST_TROp(
-            nlp,
-            TR = TR,
-            verbose = false,
-            atol = atol,
-            rtol = rtol,
-            max_time = max_time,
-            max_iter = max_iter,
-            ζ = ζ,
-        ),
+  :ARCqKOp =>
+    nlp -> ARCqKOp(
+      nlp,
+      TR = TR,
+      shifts = shifts,
+      verbose = false,
+      atol = atol,
+      rtol = rtol,
+      max_time = max_time,
+      max_iter = max_iter,
+      ζ = ζ,
+    ),
+  :ST_TROp =>
+    nlp -> ST_TROp(
+      nlp,
+      TR = TR,
+      verbose = false,
+      atol = atol,
+      rtol = rtol,
+      max_time = max_time,
+      max_iter = max_iter,
+      ζ = ζ,
+    ),
 )
 
 # Skipping unsolved problems to solve time :-)
-stats = bmark_solvers(solvers, cutest_problems, skipif = prob -> (prob.meta.name in unsolved_problems))
+stats =
+  bmark_solvers(solvers, cutest_problems, skipif = prob -> (prob.meta.name in unsolved_problems))
 
 using JLD2
 @save "paper/$(today())_$(prod(String.(keys(solvers)) .* "_"))cutest_$(string(length(problems)))_$(nmax).jld2" stats
